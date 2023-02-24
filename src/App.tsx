a364c09eaf820aea657fc1d0835f0dc8d6b26afe
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2023-02-24 11:18:08 -0300

    feat: config alias for src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { LoginPage } from './pages/LoginPage';
 
 function App() {
-  return <h1>Welcome to {import.meta.env.VITE_APP_TITLE}</h1>;
+  return (
+    <>
+      <h1>Welcome to {import.meta.env.VITE_APP_TITLE}</h1>
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
