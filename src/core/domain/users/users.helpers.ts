d387643fe96240accf0cc3a4de5e828770b3c16c
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2023-03-26 19:06:38 -0300

    fix: validate type object on isUser function

--- src/core/domain/users/users.helpers.ts
+++ src/core/domain/users/users.helpers.ts
@@ -1,7 +1,7 @@
 import { User } from './users.types';
 
 export function isUser(obj: User | null) {
-  if (obj === null) return false;
+  if (obj === null || typeof obj !== 'object') return false;
 
   return 'name' in obj && 'email' in obj && 'active' in obj && 'role' in obj;
 }
